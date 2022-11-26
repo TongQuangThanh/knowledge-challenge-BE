2702468cb6bc5fe38821657cab443acb51a0b0ef
TongQuangThanh <tongquangthanh94@gmail.com>
2022-11-26 23:01:15 +0700

    change logic achievement

--- src/routers/achievement.ts
+++ src/routers/achievement.ts
@@ -3,19 +3,13 @@ import { AchievementSchema } from '../mongoose/achievement'
 import { ERROR_SERVER, SUCCESS_ADD_ACHIEVEMENT, SUCCESS_FETCH } from '../const';
 export const achievementRouters = express.Router();
 
-achievementRouters.get('/user', (req, res) => {
-  AchievementSchema.find({ scoredBy: req.body.userId })
-    .then(result => {
-      res.status(200).json({ message: SUCCESS_FETCH, data: result })
-    })
-    .catch(err => res.status(500).json({ message: ERROR_SERVER, data: err }));
-});
-
-achievementRouters.get('/global', (req, res) => {
-  AchievementSchema.find()
-    .then(result => {
-      res.status(200).json({ message: SUCCESS_FETCH, data: result })
-    })
+achievementRouters.get('/achievement', (req, res) => {
+  let match = {};
+  if (req.body.userId) {
+    match = { scoredBy: req.body.userId };
+  }
+  AchievementSchema.find(match)
+    .then(result => res.status(200).json({ message: SUCCESS_FETCH, data: result }))
     .catch(err => res.status(500).json({ message: ERROR_SERVER, data: err }));
 });
 
